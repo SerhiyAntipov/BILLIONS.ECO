967a5c4235b2b1b74a5d6c1e4955a8dd96673930
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-02-12 19:44:41 +0200

    fixed btn bug

--- dist/js/index.js
+++ dist/js/index.js
@@ -53,8 +53,8 @@
 $(window).on('load', function () {
     let $preloader = $('.page-preloader'),
         $spinner   = $preloader.find('.spinner');
-    $spinner.delay(2000).fadeOut('slow');
-    $preloader.delay(2000).fadeOut('slow');
+    $spinner.delay(1500).fadeOut('slow');
+    $preloader.delay(1500).fadeOut('slow');
 });
 //
 //++++++++++++++ POPUP MAP WINDOW ++++++++++++++++++++++++++++++++++++++++++++++
